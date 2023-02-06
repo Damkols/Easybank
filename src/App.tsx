e1dedb360162c1d254a64ea27b6a60e579f0640d
Damkols <damolausmankolapo@gmail.com>
2023-02-06 15:30:56 +0100

    chore: few more tweaks left and unto the next one

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import "./App.scss"
 import Articles from './Articles/Articles';
+import Footer from './Footer/Footer';
 import Home from './Home/Home';
 import Navbar from './Navbar/Navbar';
 import Services from './Services/Services';
 
 const App = () => {
   return (
-    //main container
-    <div className="container">
-
-      {/* header section */}
-      <header>
+    <>
+    <header>
+      <div className="container">      
         <Navbar />
-      </header>
+      </div>
+    </header>
+    <div className="container">      
       <Home />
       <Services />
       <Articles />
+      <Footer />
     </div>
+    </>
   )
 }
 
